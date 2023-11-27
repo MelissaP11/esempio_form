d12d287b0f7189e5af7c05b27f07d12abc7d7860
Melissa <melissa.prencisvalle@iismajoranamoncalieri.edu.it>
2023-11-27 11:44:08 +0100

    form

---
+++ action.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="utf-8">
+    <title>Esempio action</title>
+</head>
+<body>
+   <h1>Recupero i dati del form</h1> 
+   <?php
+    $nome = $GET["nome"]
+    $cognome = $GET["cognome"]
+   ?>
+</body>
+</html>
