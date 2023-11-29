0cf9d068a0fe4eb812c6ac679910ebaf673653f9
Melissa <melissa.prencisvalle@iismajoranamoncalieri.edu.it>
2023-11-29 12:23:32 +0100

    cambiato metodo

--- saluto.php
+++ saluto.php
@@ -2,19 +2,20 @@
 <html lang="it">
 <head>
     <meta charset="utf-8">
-    <title>Esempio action</title>
+    <title>Saluto</title>
 </head>
 <body>
    <h1>Saluto</h1> 
    <?php
-    $nome = $_GET["nome"]; //array associativo per PHP
-    $cognome = $_GET["cognome"];
-    $sesso = $_GET ["sesso"];
+    $nome = $_POST["nome"]; //array associativo per PHP
+    $cognome = $_POST["cognome"];
+    $sesso = $_POST["sesso"];
     if($sesso == "M") {// maschio 
         print("Benvenuto $nome $cognome");
     }else { //femmina
         print("Benvenuta $nome $cognome");
     }
+    
     //print("<p>Il tuo nome è: $nome</p>");
     //print("<p>Il tuo cognome è: $cognome</p>");
    ?>
