f5e8dd49f453128edd02991a6fe5b587359c7175
Melissa <melissa.prencisvalle@iismajoranamoncalieri.edu.it>
2023-11-29 12:54:41 +0100

    aggiunto materia

---
+++ materia.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="utf-8">
+    <title>Materia</title>
+</head>
+<body>
+   <h1>Saluto con materia</h1> 
+   <?php
+    $materia = $_POST["materia"];
+    print("materia: $materia");
+   ?>
+</body>
+</html>
--- saluto.php
+++ saluto.php
@@ -15,9 +15,24 @@
     }else { //femmina
         print("Benvenuta $nome $cognome");
     }
-    
     //print("<p>Il tuo nome è: $nome</p>");
     //print("<p>Il tuo cognome è: $cognome</p>");
-   ?>
+   ?><br><br>
+   <form method="post" action="materia.php">
+    Scegli la tua materia preferita:
+    <select name="materia">
+        <option value="italiano">Italiano</option>
+        <option value="storia">Storia</option>
+        <option value="matematica">Matematica</option>
+        <option value="inglese">Inglese</option>
+        <option value="informatica">Informatica</option>
+        <option value="economia_az">Economia Aziendale</option>
+        <option value="diritto">Diritto</option>
+        <option value="economia_pol">Economia Politica</option>
+        <option value="scienze_motorie">Scienze motorie</option>
+        <option value="religione">Religione</option>
+    </select>
+    <input type="submit">
+    </form>
 </body>
 </html>
