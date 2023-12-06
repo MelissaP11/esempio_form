e1beced66ffe009b129a23bbe306a1cc15610a4e
Melissa <melissa.prencisvalle@iismajoranamoncalieri.edu.it>
2023-12-06 12:53:54 +0100

    cookie

--- materia.php
+++ materia.php
@@ -7,8 +7,8 @@
 <body>
    <h1>Saluto con materia</h1> 
    <?php
-    $nome=$_COOKIE("nome");
-    $cognome=$_COOKIE("cognome");
+    $nome=$_COOKIE["nome"];
+    $cognome=$_COOKIE["cognome"];
     $materia = $_POST["materia"];
     
     print("Ciao $nome $cognome sono molto contento che la tua materia preferita sia: $materia");
